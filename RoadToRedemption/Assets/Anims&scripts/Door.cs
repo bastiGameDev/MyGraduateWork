0fdf5574ca00c4d72e114eee60eafb49f74299fe
Andrew <basti107dark@gmail.com>
2024-03-13 13:56:06 +0300

    Реализована анимация дверей

---
+++ RoadToRedemption/Assets/Anims&scripts/Door.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour, IInterectable
+
+{
+    public Animator m_Animator;
+    public bool isOpen;
+    void Start()
+    {
+        if (isOpen)
+        {
+            m_Animator.SetBool("isOpen", true);
+        }
+    }
+
+    public string GetDescription()
+    {
+        if (isOpen) return "Нажмите [E] для <color=green>открытия</color> двери";
+        return "Нажмите [E] для <color=red>закрытия</color> двери";
+    }
+
+    public void Interact()
+    {
+        isOpen = !isOpen;
+        if (isOpen)
+            m_Animator.SetBool("isOpen", true);
+        else
+            m_Animator.SetBool("isOpen", false);
+        
+        
+    }
+
+
+}
