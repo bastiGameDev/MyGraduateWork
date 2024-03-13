35cfec3af6ff0ee4dd87c9b1ece030bd75208796
Andrew <basti107dark@gmail.com>
2024-03-13 15:03:06 +0300

    Освещение в спальне

--- RoadToRedemption/Assets/Scripts/Module_1/LightSwitch.cs
+++ RoadToRedemption/Assets/Scripts/Module_1/LightSwitch.cs
@@ -6,6 +6,7 @@ using UnityEngine.Serialization;
 public class LightSwitch : MonoBehaviour, IInterectable
 {
     [SerializeField] private Light m_Light;
+    [SerializeField] private GameObject gameObjectLight;
     [FormerlySerializedAs("_isOn")] [SerializeField] private bool isOn;
     private void Start()
     {
@@ -21,6 +22,7 @@ public class LightSwitch : MonoBehaviour, IInterectable
     public void Interact()
     {
         isOn = !isOn;
-        m_Light.enabled = isOn;
+        //m_Light.enabled = isOn;
+        gameObjectLight.SetActive(isOn);
     }
 }
