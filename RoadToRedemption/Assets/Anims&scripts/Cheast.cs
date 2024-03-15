36d68b26977fee363a2369f8d990aede2e6318eb
Andrew <basti107dark@gmail.com>
2024-03-15 10:35:03 +0300

    Исправлен баг с надписью на ящиках

--- RoadToRedemption/Assets/Anims&scripts/Cheast.cs
+++ RoadToRedemption/Assets/Anims&scripts/Cheast.cs
@@ -16,8 +16,8 @@ public class Cheast : MonoBehaviour, IInterectable
 
     public string GetDescription()
     {
-        if (isOpen) return "Нажмите [E] для <color=green>открытия</color> ящика";
-        return "Нажмите [E] для <color=red>закрытия</color> ящика";
+        if (isOpen) return "Нажмите [E] для <color=red>закрытия</color> ящика";
+        return "Нажмите [E] для <color=green>открытия</color> ящика";
     }
 
     public void Interact()
