2c07972c591006f7f1d9f4322b9df0bb34ff73f2
Andrew <basti107dark@gmail.com>
2024-03-23 16:43:46 +0300

    Реализован сташный переход на сцене 3

---
+++ RoadToRedemption/Assets/Scripts/Module1_Sc2/SecondSceneController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondSceneController : MonoBehaviour
+{
+    [SerializeField] private GameObject flyObjects;
+    [SerializeField] private AudioSource soundScary;
+
+    [SerializeField] private AudioSource creepyMusic;
+    [SerializeField] private GameObject lights;
+    
+    [SerializeField] private AudioSource environment;
+
+    private void Awake()
+    {
+        flyObjects.gameObject.SetActive(false);
+    }
+
+    void Start()
+    {
+        StartCoroutine(ScaryFold());
+    }
+
+    private IEnumerator ScaryFold()
+    {
+        Debug.Log("Coroutine is started for 10s..");
+        
+        yield return new WaitForSeconds(10);
+        
+        lights.SetActive(false);
+        
+        soundScary.Play();
+        
+        flyObjects.SetActive(true);
+        
+        environment.Pause();
+        
+        yield return new WaitForSeconds(4);
+        
+        creepyMusic.Play();
+        
+        
+        
+    }
+}
