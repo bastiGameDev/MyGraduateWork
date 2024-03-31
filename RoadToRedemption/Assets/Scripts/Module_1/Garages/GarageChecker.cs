8bfc6f27e1f0246ff78f9ead7cf62c16491b11e3
Andrew <basti107dark@gmail.com>
2024-03-31 18:23:50 +0300

    Save

--- RoadToRedemption/Assets/Scripts/Module_1/Garages/GarageChecker.cs
+++ RoadToRedemption/Assets/Scripts/Module_1/Garages/GarageChecker.cs
@@ -3,8 +3,14 @@ using UnityEngine;
 public class GarageChecker : MonoBehaviour
 {
     [SerializeField] private ActionControll actionControll;
+
+    [SerializeField] private GameObject bedMarker;
+    [SerializeField] private GameObject teapot;
     private void OnTriggerEnter(Collider other)
     {
+        bedMarker.SetActive(true);
+        teapot.SetActive(false);
+        
         actionControll.EndingGarage();
     }
 }
