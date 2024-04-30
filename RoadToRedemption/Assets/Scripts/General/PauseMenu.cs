f2a8a87fa8ae61be3347ae0263b9ed486b2dcb4e
Andrew <basti107dark@gmail.com>
2024-04-30 18:28:39 +0300

    откывание меню

---
+++ RoadToRedemption/Assets/Scripts/General/PauseMenu.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    private static bool _isPaused = false;
+    [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private GameObject fpsController;
+    [SerializeField] private GameObject fpsCamera;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    
+    private void ShowCursor(bool show)
+    {
+        Cursor.visible = show;
+        Cursor.lockState = show ? CursorLockMode.None : CursorLockMode.Locked;
+    }
+
+    private void Resume()
+    {
+        fpsCamera.GetComponent<FirstPersonLook>().enabled = true;
+        fpsCamera.GetComponent<PickUpObject>().enabled = true;
+        fpsController.GetComponent<PlayerInteraction>().enabled = true;
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        _isPaused = false;
+        ShowCursor(false);
+    }
+
+    private void Pause()
+    {
+        fpsCamera.GetComponent<FirstPersonLook>().enabled = false;
+        fpsCamera.GetComponent<PickUpObject>().enabled = false;
+        fpsController.GetComponent<PlayerInteraction>().enabled = false;
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        _isPaused = true;
+        ShowCursor(true);
+    }
+}
