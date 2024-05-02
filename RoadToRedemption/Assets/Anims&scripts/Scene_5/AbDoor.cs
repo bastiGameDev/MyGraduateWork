8a4fb8036edd74aa7ecb9524c48cbca6dc0168ca
Andrew <basti107dark@gmail.com>
2024-05-02 13:41:01 +0300

    Анимация одной стороны дверей

---
+++ RoadToRedemption/Assets/Anims&scripts/Scene_5/AbDoor.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbDoor : MonoBehaviour, IInterectable
+{
+    [SerializeField] private AudioSource soundDoorOpen;
+    
+    public Animator m_Animator;
+    public bool isOpen;
+    void Start()
+    {
+        if (isOpen)
+        {
+            m_Animator.SetBool("isOpen", true);
+        }
+    }
+
+    public string GetDescription()
+    {
+        if (isOpen) return "Нажмите для <color=red>закрытия</color> двери";
+        return "Нажмите для <color=green>открытия</color> двери";
+    }
+
+    public void Interact()
+    {
+        soundDoorOpen.Play();
+        
+        isOpen = !isOpen;
+        if (isOpen)
+            m_Animator.SetBool("isOpen", true);
+        else
+            m_Animator.SetBool("isOpen", false);
+        
+        
+    }
+
+
+}
